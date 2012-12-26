ac536848c3c214f97feae6008febd96dfd5a1541
Charles Magahern <charles@magahern.com>
2012-12-26 15:47:37 -0700

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ * Author: Charles Magahern <charles@magahern.com>
+ * Date Created: 12/26/2012
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
